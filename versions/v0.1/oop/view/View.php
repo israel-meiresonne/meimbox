8f82f692e5c982c83c456b88a78bcf2f839b1787
Israel Meiresonne <israelmeiresonne.iandmeim@gmail.com>
2020-09-01 14:15:51 +0200

    update v0.1's 'base' tag for layout

--- versions/v0.1/oop/view/View.php
+++ versions/v0.1/oop/view/View.php
@@ -81,7 +81,8 @@ class View implements ViewInterface
             '<meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <base href="/v1.0/">
+            <!-- <base href="/v1.0/"> --!>
+            <base href="/versions/v0.1/">
             <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> --!>
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
