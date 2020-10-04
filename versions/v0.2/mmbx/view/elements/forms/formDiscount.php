2e58fd379962fe6bf5882367a0eeeb2f2c9edcfb
Israel Meiresonne <israelmeiresonne97@gmail.com>
2020-10-04 19:34:21 +0200

    end fixe display of checkout page

---
+++ versions/v0.2/mmbx/view/elements/forms/formDiscount.php
@@ -0,0 +1,100 @@
+<hr class="hr-summary">
+<div class="cart-discount-block">
+    <div class="form-title-block discount-title-block">
+        <div class="form-title-div">
+            <span class="form-title">discount codes</span>
+        </div>
+        <div class="summary-detail-arrow-button-div">
+            <div class="arrow-element-container">
+                <div class="arrow-element-wrap">
+                    <span class="arrow-span"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="discount_body" class="cart-discount-inner">
+        <hr class="hr-summary">
+        <div class="cart-discount-input-block">
+            <div class="input_button_left-wrap">
+                <div class="input_button_left-intput-block">
+                    <div class="input-wrap">
+                        <label class="input-label" for="cart_discount">discount code</label>
+                        <input id="cart_discount" class="input-tag" type="text" name="discount" placeholder="discount code" value="">
+                        <p class="comment"></p>
+                    </div>
+                </div>
+                <div class="input_button_left-button-block">
+                    <button class="green-button standard-button remove-button-default-att">apply</button>
+                </div>
+            </div>
+        </div>
+        <hr class="hr-summary">
+        <div class="sticker-set">
+            <div class="sticker-container">
+                <div class="sticker-wrap">
+                    <div class="sticker-content-div">jackets</div>
+                    <button class="sticker-button remove-button-default-att">
+                        <span class="sticker-x-left"></span>
+                        <span class="sticker-x-right"></span>
+                    </button>
+                </div>
+            </div>
+            <div class="sticker-container">
+                <div class="sticker-wrap">
+                    <div class="sticker-content-div">jackets</div>
+                    <button class="sticker-button remove-button-default-att">
+                        <span class="sticker-x-left"></span>
+                        <span class="sticker-x-right"></span>
+                    </button>
+                </div>
+            </div>
+            <div class="sticker-container">
+                <div class="sticker-wrap">
+                    <div class="sticker-content-div">jackets</div>
+                    <button class="sticker-button remove-button-default-att">
+                        <span class="sticker-x-left"></span>
+                        <span class="sticker-x-right"></span>
+                    </button>
+                </div>
+            </div>
+            <div class="sticker-container">
+                <div class="sticker-wrap">
+                    <div class="sticker-content-div">jackets</div>
+                    <button class="sticker-button remove-button-default-att">
+                        <span class="sticker-x-left"></span>
+                        <span class="sticker-x-right"></span>
+                    </button>
+                </div>
+            </div>
+            <div class="sticker-container">
+                <div class="sticker-wrap">
+                    <div class="sticker-content-div">jackets</div>
+                    <button class="sticker-button remove-button-default-att">
+                        <span class="sticker-x-left"></span>
+                        <span class="sticker-x-right"></span>
+                    </button>
+                </div>
+            </div>
+            <div class="sticker-container">
+                <div class="sticker-wrap">
+                    <div class="sticker-content-div">jackets</div>
+                    <button class="sticker-button remove-button-default-att">
+                        <span class="sticker-x-left"></span>
+                        <span class="sticker-x-right"></span>
+                    </button>
+                </div>
+            </div>
+            <div class="sticker-container">
+                <div class="sticker-wrap">
+                    <div class="sticker-content-div">jackets</div>
+                    <button class="sticker-button remove-button-default-att">
+                        <span class="sticker-x-left"></span>
+                        <span class="sticker-x-right"></span>
+                    </button>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
