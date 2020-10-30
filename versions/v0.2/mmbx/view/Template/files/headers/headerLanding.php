e0bb82b505e76830ae0e30d93283a4fbb0f4a792
Israel Meiresonne <israelmeiresonne.iandmeim@gmail.com>
2020-10-30 12:50:26 +0100

    add value proposition (vp) section in landing page

--- versions/v0.2/mmbx/view/Template/files/headers/headerLanding.php
+++ versions/v0.2/mmbx/view/Template/files/headers/headerLanding.php
@@ -1,1 +1,16 @@
-header
+<?php
+/**
+ * ——————————————————————————————— NEED —————————————————————————————————————
+ * @param Map $companyMap holds datas about company
+ */
+/**
+ * @var Map */
+$companyMap = $companyMap;
+?>
+<header class="header_landing">
+    <div class="header_content">
+        <div class="header_content-brand">
+            <span><?= $companyMap->get(Map::brand) ?></span>
+        </div>
+    </div>
+</header>
