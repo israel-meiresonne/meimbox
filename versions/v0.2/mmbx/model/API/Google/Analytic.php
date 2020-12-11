d74a021a58b6e43f104ac6e6fbb135e08843f717
Israel Meiresonne <israelmeiresonne.iandmeim@gmail.com>
2020-12-11 23:13:56 +0100

    set up config file 'prod.ini'

--- versions/v0.2/mmbx/model/API/Google/Analytic.php
+++ versions/v0.2/mmbx/model/API/Google/Analytic.php
@@ -6,11 +6,11 @@ require_once 'model/ModelFunctionality.php';
  */
 class Analytic extends ModelFunctionality
 {
-    /**
-     * Holds Google's measure ID
-     * @var string
-     */
-    private const TAG_ID = "G-9NZD679DX8";
+    // /**
+    //  * Holds Google's measure ID
+    //  * @var string
+    //  */
+    // private const TAG_ID = "G-9NZD679DX8";
 
     /**
      * To get Google Analytic's base code
@@ -18,8 +18,9 @@ class Analytic extends ModelFunctionality
      */
     public static function getBaseCode()
     {
-        $env = Configuration::getEnvironement();
-        $tagID = ($env == Configuration::ENV_PROD) ? self::TAG_ID : Configuration::get(Configuration::GOOGLE_MEASURE_ID);
+        // $env = Configuration::getEnvironement();
+        // $tagID = ($env == Configuration::ENV_PROD) ? self::TAG_ID : Configuration::get(Configuration::GOOGLE_MEASURE_ID);
+        $tagID = Configuration::get(Configuration::GOOGLE_MEASURE_ID);
         return self::generateFile('model/API/Google/files/analyticBaseCode.php', ["tagID" => $tagID]);
     }
 }
